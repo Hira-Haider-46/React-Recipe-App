c3d7261c11e36a8337db66415f45ad0b25b6d670
Hira <hirahaiderali1975@gmail.com>
2024-09-19 17:58:22 +0500

    footer added

--- src/CommonComponents/Footer/Footer.jsx
+++ src/CommonComponents/Footer/Footer.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import Button from '../Button';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import './Footer.css';
 
 function Footer() {
-  return (
-    <footer>
-        <section></section>
-        <section></section>
-        <section></section>
-    </footer>
-  )
+    return (
+        <footer>
+            <section className='email-section'>
+                <h1>Get recipes straight to your inbox!</h1>
+                <div>
+                    <input type="text" placeholder='Email address' />
+                    <Button text='Subscribe' />
+                </div>
+            </section>
+            <section className='socials-section'>
+                <h2><Link to="/">Recipe App</Link></h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dolorem sequi ex voluptatibus ab adipisci officiis, a perferendis aut consectetur quo soluta libero expedita maiores. Ab voluptatem alias obcaecati dolore!</p>
+                <div className='social-icons'>
+                    <FaInstagram />
+                    <FaYoutube />
+                    <FaFacebookF />
+                    <FaTwitter />
+                </div>
+            </section>
+            <section className='footer-section'>
+                <p>© Recipe App 2024. All Rights Reserved.</p>
+            </section>
+        </footer>
+    )
 }
 
 export default Footer;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../CommonComponents/Navbar";
+import Footer from "../../CommonComponents/Footer";
 
 export default function Layout() {
     return (
         <>
             <Navbar />
             <Outlet />
+            <Footer />
         </>
     )
 };
