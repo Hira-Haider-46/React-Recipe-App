7378d5cdc59fbd6804316e13b6a607d81991bb4f
Hira <hirahaiderali1975@gmail.com>
2024-09-20 15:10:59 +0500

    updated recipe page

--- src/Pages/Recipes/Recipes.jsx
+++ src/Pages/Recipes/Recipes.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import RecipeCard from '../../CommonComponents/RecipeCard';
-import Search from '../../Components/Search';
+import { CiSearch } from "react-icons/ci";
 import './Recipes.css';
 
-function Recipes() {
+export default function Recipes() {
   return (
     <div className='recipes'>
       <h1>All recipes</h1>
-      <Search />
+      <div className='search-wrapper'>
+        <CiSearch className='search-icon' />
+        <input type="text" placeholder='Search for a recipe' />
+      </div>
       <div className='recipes-container'>
         <RecipeCard />
       </div>
     </div>
   )
 }
-
-export default Recipes;
