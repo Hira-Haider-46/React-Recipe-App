2e42a7aabd345231090d4b06fd359c198511596c
Hira <hirahaiderali1975@gmail.com>
2024-09-21 21:05:01 +0500

    responsiveness added in header Footer

--- src/CommonComponents/FeaturedIn/FeaturedIn.jsx
+++ src/CommonComponents/FeaturedIn/FeaturedIn.jsx
@@ -15,7 +15,7 @@ function FeaturedIn() {
         <img src='https://cdn.prod.website-files.com/63a595b668385c4de9a03b27/63a5ab3d85e33d3b02335fd6_Logo%20VegNews.svg' alt='brand-logo' />
         <img src='https://cdn.prod.website-files.com/63a595b668385c4de9a03b27/63a5ab3d78e8ccee25d37667_Logo%20Buzzfeed.svg' alt='brand-logo' />
         <img src='https://cdn.prod.website-files.com/63a595b668385c4de9a03b27/63a5ab3dc97e6a6494313230_Logo%20Huffpost.svg' alt='brand-logo' />
-        <img src='https://cdn.prod.website-files.com/63a595b668385c4de9a03b27/63a595b668385c4de9a03b27/63a5ab3d65217b43af1f7781_Logo%20Forks%20Over%20Knives.svg' alt='brand-logo' />
+        <img src='https://cdn.prod.website-files.com/63a595b668385c4de9a03b27/63a5ab3d65217b43af1f7781_Logo%20Forks%20Over%20Knives.svg' alt='brand-logo' />
       </div>
     </div>
   );
