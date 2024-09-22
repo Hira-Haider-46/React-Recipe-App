96f280c08ef2ecafe6733e2a73becbcd65a82f38
Hira <hirahaiderali1975@gmail.com>
2024-09-22 16:31:23 +0500

    navbar issue fixed

--- src/CommonComponents/Navbar/Navbar.jsx
+++ src/CommonComponents/Navbar/Navbar.jsx
@@ -4,24 +4,12 @@ import './Navbar.css';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
-    const [showMenu, setShowMenu] = useState(true);
 
     const toggleMenu = () => {
-        if (!isOpen) {
-            setIsOpen(true);
-            setTimeout(() => {
-                setShowMenu(true);
-            }, 700); 
-        } else {
-            setShowMenu(false);
-            setTimeout(() => {
-                setIsOpen(false);
-            }, 300);
-        }
+        setIsOpen(!isOpen);
     };
 
     const handleLinkClick = () => {
-        setShowMenu(false);
         setIsOpen(false);
     };
 
@@ -29,7 +17,6 @@ function Navbar() {
         const handleResize = () => {
             if (window.innerWidth > 1000) {
                 setIsOpen(false); 
-                setShowMenu(true);
             }
         };
 
@@ -39,26 +26,24 @@ function Navbar() {
 
     return (
         <nav>
-            <h1><Link to="/">Recipe App</Link></h1>
+            <h1><Link to="/" onClick={handleLinkClick}>Recipe App</Link></h1>
             <div className={`menu-toggle ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
             <div className={`bg ${isOpen ? 'open' : ''}`}></div>
-            {showMenu && (
-                <ul className={isOpen ? 'open' : ''}>
-                    <li>
-                        <Link to="/recipes" onClick={handleLinkClick}>Recipes</Link>
-                    </li>
-                    <li>
-                        <Link to="/about" onClick={handleLinkClick}>About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" onClick={handleLinkClick}>Contact</Link>
-                    </li>
-                </ul>
-            )}
+            <ul className={isOpen ? 'open' : ''}>
+                <li>
+                    <Link to="/recipes" onClick={handleLinkClick}>Recipes</Link>
+                </li>
+                <li>
+                    <Link to="/about" onClick={handleLinkClick}>About</Link>
+                </li>
+                <li>
+                    <Link to="/contact" onClick={handleLinkClick}>Contact</Link>
+                </li>
+            </ul>
         </nav>
     );
 }
